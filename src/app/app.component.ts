25d0791b933970f6f730902d8019251af9d1f444
Mli <mliotard@yahoo.fr>
2024-10-16 22:23:45 +0200

    Store data querying, plain.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import {select, Store} from "@ngrx/store";
 import {Observable} from "rxjs";
 import {map} from 'rxjs/operators';
 import {NavigationCancel, NavigationEnd, NavigationError, NavigationStart, Router} from '@angular/router';
+import { AuthState } from './auth/reducers';
 
 @Component({
   selector: 'app-root',
@@ -12,13 +13,27 @@ import {NavigationCancel, NavigationEnd, NavigationError, NavigationStart, Route
 export class AppComponent implements OnInit {
 
     loading = true;
+    isLoggedIn$: Observable<boolean>;
+    isLoggedOut$: Observable<boolean>;
 
-    constructor(private router: Router) {
+    constructor(private router: Router, private store: Store<AuthState>) {
 
     }
 
     ngOnInit() {
 
+      //this.store.subscribe(state => console.log("store value:", state));
+
+      this.isLoggedIn$ = this.store
+      .pipe(
+        map(state => !!state["auth"].user)
+      );
+      this.isLoggedOut$ = this.store
+      .pipe(
+        map(state => !state["auth"].user)
+      );
+
+      
       this.router.events.subscribe(event  => {
         switch (true) {
           case event instanceof NavigationStart: {
